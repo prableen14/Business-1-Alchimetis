76252f279f354d3c9cd3cd13ceda4a13b7f0097d
Sarah <vongocquynhnhu@gmail.com>
2023-10-06 09:35:25 -0400

    create more layouts

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,6 +1,6 @@
 // export atoms created
 import Title from "./Title/title.js";
-import Profile from "./Profile/profile";
+import Profile from "./Profile/profile.jsx";
 import TextAndImage from "./TextAndImage/textAndImage.js";
 import MessagePreview from "./MessagePreview/messagePreview.js";
 
--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -2,31 +2,76 @@ import React from "react";
 import { Title, TextAndImage, MessagePreview } from "../../atoms/index.js";
 import homeSvg from "../../assets/images/HomeDark.svg";
 import { RevenueHistogram } from "../../atoms/RevenueHistogram";
+import "./style.scss";
 
 const Dashboard = (props) => {
   return (
     <div>
       <Title title="Dashboard" size="large" subtitle={"Welcome back, Rahul"} />
       <Title title="Recent Messages" size="medium" />
-      <TextAndImage
-        imgSrc={homeSvg}
-        title="Choose perfect product idea"
-        date="Jan 18"
-        time="5:00 pm"
-      />
-      <MessagePreview
-        imgSrc="https://i.pinimg.com/originals/17/e1/7e/17e17e558fe2269e93b0412182301a69.jpg"
-        title="Jenny Fox"
-        message="Update on marketing campaign"
-        time="2:32pm"
-      />
-      <MessagePreview
-        imgSrc="https://i.pinimg.com/originals/17/e1/7e/17e17e558fe2269e93b0412182301a69.jpg"
-        title="Jenny Fox"
-        message="Update on marketing campaign"
-        time="2:32pm"
-      />
+
       <RevenueHistogram />
+
+      <div className="notification">
+        <div className="notification__chat">
+          <MessagePreview
+            imgSrc="https://i.pinimg.com/originals/17/e1/7e/17e17e558fe2269e93b0412182301a69.jpg"
+            name="Jenny Fox"
+            message="Update on marketing campaign"
+            time="2:32pm"
+          />
+          <MessagePreview
+            imgSrc="https://i.pinimg.com/originals/17/e1/7e/17e17e558fe2269e93b0412182301a69.jpg"
+            name="Jenny Fox"
+            message="Update on marketing campaign"
+            time="2:32pm"
+          />
+          <MessagePreview
+            imgSrc="https://i.pinimg.com/originals/17/e1/7e/17e17e558fe2269e93b0412182301a69.jpg"
+            name="Jenny Fox"
+            message="Update on marketing campaign"
+            time="2:32pm"
+          />
+          <MessagePreview
+            imgSrc="https://i.pinimg.com/originals/17/e1/7e/17e17e558fe2269e93b0412182301a69.jpg"
+            name="Jenny Fox"
+            message="Update on marketing campaign"
+            time="2:32pm"
+          />
+        </div>
+        <div className="notification__todo">
+          <TextAndImage
+            imgSrc={homeSvg}
+            title="Choose perfect product idea"
+            date="Jan 18"
+            time="5:00 pm"
+          />
+          <TextAndImage
+            imgSrc={homeSvg}
+            title="Choose perfect product idea"
+            date="Jan 18"
+            time="5:00 pm"
+          />
+          <TextAndImage
+            imgSrc={homeSvg}
+            title="Choose perfect product idea"
+            date="Jan 18"
+            time="5:00 pm"
+          />
+          <TextAndImage
+            imgSrc={homeSvg}
+            title="Choose perfect product idea"
+            date="Jan 18"
+            time="5:00 pm"
+          />
+          <TextAndImage
+            imgSrc={homeSvg}
+            title="Choose perfect product idea"
+            date="Jan 18"
+            time="5:00 pm"
+          />
+        </div>
+      </div>
     </div>
   );
 };
