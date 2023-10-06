d3655dabc16766d8ce93c2a1664389acfc832eca
Megha Bhargava <megha.bhargav2012@gamil.com>
2023-10-06 00:01:55 -0400

    resolve conflict

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Title, TextAndImage, MessagePreview } from "../../atoms/index.js";
-import homeSvg from '../../assets/Home.svg'
+import homeSvg from "../../assets/images/HomeDark.svg";
 
 const Dashboard = (props) => {
   return (
     <div>
       <Title title='Dashboard' size='large' subtitle={"Welcome back, Rahul"} />
       <Title title='Recent Messages' size='medium' />
-      <TextAndImage imgSrc={homeSvg} title='Choose perfect product idea' date='Jan 18' time='5:00 pm'/>
+      <TextAndImage
+        imgSrc={homeSvg}
+        title='Choose perfect product idea'
+        date='Jan 18'
+        time='5:00 pm'
+      />
       <MessagePreview
         imgSrc='https://i.pinimg.com/originals/17/e1/7e/17e17e558fe2269e93b0412182301a69.jpg'
         title='Jenny Fox'
