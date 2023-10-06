fbd66e1b1150269f1ea1a2fd09599b4c1c830b65
lws803 <lws803@gmail.com>
2023-10-05 21:47:29 -0400

    added styles and histogram component

--- src/atoms/RevenueHistogram/index.js
+++ src/atoms/RevenueHistogram/index.js
@@ -1,17 +1,21 @@
 import React from "react";
-import charts from "apexcharts";
 
 import "./style.scss";
 
 import UpTrend from "../../assets/Up Trend.svg";
 
 export function RevenueHistogram() {
+  const values = [50, 60, 30, 70, 80, 90, 100, 20, 10];
+  const revenue = 8_900;
+
   return (
     <div className="revenue-histogram">
       <div className="revenue-histogram-header">
-        <div className="revenue-histogram-value">
-          <div>Revenue</div>
-          <div>$8900.70</div>
+        <div className="revenue-histogram-header-value-wrapper">
+          <div className="revenue-histogram-header-title">Revenue</div>
+          <div className="revenue-histogram-header-value">
+            ${revenue.toLocaleString()}
+          </div>
         </div>
 
         <img
@@ -19,6 +23,17 @@ export function RevenueHistogram() {
           alt="Up trend icon"
           className="revenue-histogram-header-icon"
         />
+      </div>
+
+      <div className="revenue-histogram-chart">
+        {values.map((values, index) => (
+          <div className="revenue-histogram-chart-bar-container" key={index}>
+            <div
+              className="revenue-histogram-chart-bar-container-bar"
+              style={{ height: `${values}%` }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
