28caa493c057d5268d8ccaac6b6cc3573d755bfe
Prableen Kaur <prableenkaur2016@gmail.com>
2023-10-06 00:17:45 -0400

    Filter component added

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,4 +1,6 @@
 // export atoms created
 import Title from "./Title/title.js";
-
+import Filter from "./Filter/filter.js";
 export { Title };
+export {Filter};
+
--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Title } from "../../atoms/index.js";
-
+import {Filter} from "../../atoms/index.js";
 const Dashboard = (props) => {
   return (
     <div>
+    <Filter/>
       <Title title='Dashboard' size='large' subtitle={"Welcome back, Rahul"} />
       <Title title='Recent Messages' size='medium' />
     </div>
