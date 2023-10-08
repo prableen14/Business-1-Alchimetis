451c70f59da15a0e41ea9663633dd03c4b9dde1f
flynn_pham <flynn_pham@Flynns-MacBook-Pro.local>
2023-10-07 21:41:10 -0400

    Setup dashboard banner

--- src/atoms/Profile/profile.jsx
+++ src/atoms/Profile/profile.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./style.scss";
 
-const Profile = () => {
+const Profile = (prop) => {
+  const {name="Rahul Shaw", email="rahulshaw@gmail.com"} = prop
   return (
     <div className='profile-container'>
       <div className='profile-container_avatar'>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtwHGbBun5jLcARrAqEug83OeGrSmECj4pEg&usqp=CAU" alt="placeholder" />
       </div>
       <div className='profile-container_info'>
-        <div className='profile-container_info-name'>name</div>
-        <div className='profile-container_info-email'>email</div>
+        <div className='profile-container_info-name'>{name}</div>
+        <div className='profile-container_info-email'>{email}</div>
       </div>
     </div>
   )
