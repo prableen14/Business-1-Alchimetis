8efecb7567a461091823b6609692967f91dbe6c6
Megha Bhargava <megha.bhargav2012@gamil.com>
2023-10-27 09:42:09 -0400

    Update login.js

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,31 +1,40 @@
 import React from "react";
 import "./style.scss";
 import { Title, Textbox, Button } from "../../atoms";
+import Form from "react-bootstrap/Form";
 import LeafBlue from "../../assets/images/companyLeafBlue.svg";
 
 const Login = (props) => {
   return (
-    <div className="Login-Page">
-      <div className="Login-Page-box">
-        <div id="login-header">
-          <img src={LeafBlue} alt="company leaf blue" />{" "}
-          <Title title="Ecoforce" size="large" />
+    <div className='Login-Page'>
+      <div className='Login-Page-box'>
+        <div id='login-header'>
+          <img src={LeafBlue} alt='company leaf blue' />{" "}
+          <Title title='Ecoforce' size='large' />
         </div>
         <div>
           <Textbox
             placeholder={"Enter username"}
-            label="Username"
-            name="username"
+            label='Username'
+            name='username'
           />
           <Textbox
             placeholder={"Enter password"}
-            label="Password"
-            type="password"
-            name="password"
+            label='Password'
+            type='password'
+            name='password'
           />
         </div>
-        <a href="/dashboard">
-          <Button text="Login" />
+        <div className='login-terms'>
+          <Form.Check
+            type={"checkbox"}
+            id={`default-checkbox`}
+            className='term-checkbox'
+          />
+          <span>Remember me</span>
+        </div>
+        <a href='/dashboard'>
+          <Button text='Login' />
         </a>
       </div>
     </div>
