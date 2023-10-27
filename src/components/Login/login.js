e50ab4b418a90708989153ffa7134e4a9b2555d3
lws803 <lws803@gmail.com>
2023-10-27 08:50:33 -0400

    added auth flow

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -5,19 +5,28 @@ import LeafBlue from "../../assets/images/companyLeafBlue.svg";
 
 const Login = (props) => {
   return (
-    <div className='Login-Page'>
-      <div className='Login-Page-box'>
-        <div id='login-header'>
-          <img src={LeafBlue} alt='company leaf blue' />{" "}
-          <Title title='Ecoforce' size='large' />
+    <div className="Login-Page">
+      <div className="Login-Page-box">
+        <div id="login-header">
+          <img src={LeafBlue} alt="company leaf blue" />{" "}
+          <Title title="Ecoforce" size="large" />
         </div>
         <div>
-          <Textbox placeholder={"Enter username"} label='Username' />
-          <Textbox placeholder={"Enter password"} label='Password' />
+          <Textbox
+            placeholder={"Enter username"}
+            label="Username"
+            name="username"
+          />
+          <Textbox
+            placeholder={"Enter password"}
+            label="Password"
+            type="password"
+            name="password"
+          />
         </div>
-        <div>
-          <Button text='Login' />
-        </div>
+        <a href="/dashboard">
+          <Button text="Login" />
+        </a>
       </div>
     </div>
   );
