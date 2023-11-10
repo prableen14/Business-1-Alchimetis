13981d42a708c22dd72a7c7ae612d8b14c6d6e24
Megha Bhargava <megha.bhargav2012@gamil.com>
2023-11-09 19:50:29 -0500

    multiform graph

--- src/atoms/MultiFormGraph/multiFormGraph.js
+++ src/atoms/MultiFormGraph/multiFormGraph.js
@@ -1,74 +1,43 @@
 import React from "react";
 import {
-  BarChart,
   Bar,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
-  LineChart,
   Line,
-  AreaChart,
   Area,
   ComposedChart,
 } from "recharts";
 
-const MultiFormGraph = (props) => {
-  const data = props?.data || [];
+const MultiFormGraph = ({ data, charts }) => {
   return (
-    <div>
-      <BarChart width={800} height={400} data={data}>
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='Month' />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey='Actual' fill='#8884d8' />
-        {/* Add more bars for other properties if needed */}
-      </BarChart>
-
-      <AreaChart width={800} height={400} data={data}>
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='Month' />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Area
-          type='monotone'
-          dataKey='Accrued'
-          stroke='#82ca9d'
-          fill='#82ca9d'
-        />
-        {/* Add more areas for other properties if needed */}
-      </AreaChart>
-
-      <LineChart width={800} height={400} data={data}>
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='Month' />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type='monotone' dataKey='Estimated' stroke='#ffc658' />
-        {/* Add more lines for other properties if needed */}
-      </LineChart>
-      <ComposedChart width={800} height={400} data={data}>
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='Month' />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey='Actual' fill='#8884d8' />
-        <Area
-          type='monotone'
-          dataKey='Accrued'
-          stroke='#82ca9d'
-          fill='#82ca9d'
-        />
-        {/* <Line type='monotone' dataKey='Estimated' stroke='#ffc658' /> */}
-        {/* Add more bars, areas, or lines for other properties if needed */}
-      </ComposedChart>
-    </div>
+    <ComposedChart width={800} height={400} data={data}>
+      <XAxis dataKey='Month' />
+      <YAxis />
+      <CartesianGrid strokeDasharray='3 3' />
+      <Tooltip />
+      <Legend />
+      {charts?.map((chart) => {
+        if (chart.type === "bar") {
+          return <Bar dataKey={chart.dataKey} fill='#8884d8' />;
+        } else if (chart.type === "line") {
+          return (
+            <Line type='monotone' dataKey={chart.dataKey} stroke='#82ca9d' />
+          );
+        } else if (chart.type === "area") {
+          return (
+            <Area
+              type='monotone'
+              dataKey={chart.dataKey}
+              stroke='#ffc658'
+              fill='#ffc658'
+            />
+          );
+        }
+      })}
+    </ComposedChart>
   );
 };
 
--- src/components/Report/reportEnvironment.js
+++ src/components/Report/reportEnvironment.js
@@ -2,49 +2,63 @@ import React, { useEffect, useState } from "react";
 import "./style.scss";
 import Summary from "../../data/Summary.json";
 import { GraphCard } from "../../molecules/index.js";
-import { AreaGraph, LineGraph, PieChart, BarGraph } from "../../atoms/index.js";
+import {
+  AreaGraph,
+  LineGraph,
+  PieChart,
+  BarGraph,
+  MultiFormGraph,
+} from "../../atoms/index.js";
 import { getDataByType, getLatestData } from "../Utils/utils.js";
 
-const ReportEnvironment = ({data}) => {
-  const [finalData, setFinalData] = useState([])
+const ReportEnvironment = ({ data }) => {
+  const [finalData, setFinalData] = useState([]);
 
-  useEffect(() =>{
-    if (!data) return
+  useEffect(() => {
+    if (!data) return;
 
     const groupedData = data.reduce((groups, item) => {
       const key = `${item.type}-${item.category}-${item.groupBy}`;
-    
-      if (!groups[key] || item.createdDate.seconds > groups[key].createdDate.seconds) {
+
+      if (
+        !groups[key] ||
+        item.createdDate.seconds > groups[key].createdDate.seconds
+      ) {
         groups[key] = item;
       }
-    
+
       return groups;
     }, {});
-    
+
     // Convert the grouped data back to an array
     const result = Object.values(groupedData);
-    setFinalData(result)
+    setFinalData(result);
 
     //sample how to get the data out
-    console.log('sample: ', getDataByType(result, 'e', 'co2', 'groups'))
-  },[])
+    console.log("sample: ", getDataByType(result, "e", "co2", "groups"));
+  }, []);
 
-  
   return (
     <div className='reportEnvironment'>
       <div className='reportEnvironment-group'>
         <div className='reportEnvironment-block-title'>CO2</div>
         <div className='reportEnvironment-block'>
+          <MultiFormGraph
+            data={Summary}
+            charts={[
+              { dataKey: "Accrued", type: "line" },
+              { dataKey: "Estimated", type: "bar" },
+            ]}
+          />
           <GraphCard
             content={
               <AreaGraph
                 data={Summary}
                 dataKey={"Accrued"}
                 xAxisDataKey={"Month"}
-                styles={{ fill: '#0bb197', stroke: '#0bb197' }}
+                styles={{ fill: "#0bb197", stroke: "#0bb197" }}
               />
             }
-            //   sublabel='esimation'
             label='Activity by groups'
           />
           <GraphCard
@@ -53,34 +67,32 @@ const ReportEnvironment = ({data}) => {
                 data={Summary}
                 dataKey={"Accrued"}
                 xAxisDataKey={"Month"}
-                styles={{ fill: '#0bb197', stroke: '#0bb197' }}
+                styles={{ fill: "#0bb197", stroke: "#0bb197" }}
               />
             }
-            //   sublabel='esimation'
             label='Activity by groups'
           />
         </div>
         <div className='reportEnvironment-block'>
-        <GraphCard
+          <GraphCard
             content={
               <PieChart
-                data={getLatestData(data, 'e', 'co2', 'scope').data}
-                dataKey={'proportion'}
-                name={'measures'}
+                data={getLatestData(data, "e", "co2", "scope").data}
+                dataKey={"proportion"}
+                name={"measures"}
                 label={true}
               />
             }
-            //   sublabel='esimation'
             label='Activity by scope'
           />
           <GraphCard
             content={
               <BarGraph
-                data={getLatestData(data, 'e', 'co2', 'period').data}
-                dataKey={'co2'}
-                name={'measures'}
+                data={getLatestData(data, "e", "co2", "period").data}
+                dataKey={"co2"}
+                name={"measures"}
                 twoBars={true}
-                dataKey2={'co2Prev'}
+                dataKey2={"co2Prev"}
                 radius={0}
               />
             }
@@ -90,7 +102,12 @@ const ReportEnvironment = ({data}) => {
         </div>
       </div>
       <div className='reportEnvironment-group'>
-        <div className='reportEnvironment-block-title' style={{ color: '#564ab1', borderColor: '#564ab1' }}>Energy</div>
+        <div
+          className='reportEnvironment-block-title'
+          style={{ color: "#564ab1", borderColor: "#564ab1" }}
+        >
+          Energy
+        </div>
         <div className='reportEnvironment-block'>
           <GraphCard
             content={
@@ -98,7 +115,7 @@ const ReportEnvironment = ({data}) => {
                 data={Summary}
                 dataKey={"Estimated"}
                 xAxisDataKey={"Month"}
-                styles={{ fill: '#564ab1', stroke: '#564ab1' }}
+                styles={{ fill: "#564ab1", stroke: "#564ab1" }}
               />
             }
             //   sublabel='esimation'
