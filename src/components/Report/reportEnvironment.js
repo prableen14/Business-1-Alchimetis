915eaf68b15443a078cb07acc686237bdf212fa0
Megha Bhargava <megha.bhargav2012@gamil.com>
2023-11-09 23:31:43 -0500

    Update reportEnvironment.js

--- src/components/Report/reportEnvironment.js
+++ src/components/Report/reportEnvironment.js
@@ -47,8 +47,8 @@ const ReportEnvironment = ({ data }) => {
               <MultiFormGraph
                 data={getLatestData(data, "e", "co2", "groups").data}
                 charts={[
-                  { dataKey: "co2", type: "line" },
-                  { dataKey: "co2Prev", type: "bar" },
+                  { dataKey: "co2", type: "bar" },
+                  { dataKey: "co2Prev", type: "line" },
                 ]}
                 xaxisDataKey={"group"}
               />
@@ -78,7 +78,7 @@ const ReportEnvironment = ({ data }) => {
             /> */}
         </div>
         <div className='reportEnvironment-block'>
-        <GraphCard
+          <GraphCard
             content={
               <PieChart
                 data={getLatestData(data, "e", "co2", "location").data}
@@ -102,7 +102,6 @@ const ReportEnvironment = ({ data }) => {
             }
             label='Activity by Locations - Current and Previous CO2'
           />
-     
         </div>
 
         <div className='reportEnvironment-block'>
