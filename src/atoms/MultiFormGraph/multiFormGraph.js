75a5ba57ea80c4ac28b7dd2499cb32513a11c367
lws803 <lws803@gmail.com>
2023-11-16 11:44:36 -0500

    added better legend label for co2Prev

--- src/atoms/MultiFormGraph/multiFormGraph.js
+++ src/atoms/MultiFormGraph/multiFormGraph.js
@@ -16,33 +16,41 @@ const formatter = Intl.NumberFormat("en", { notation: "compact" });
 
 const MultiFormGraph = ({ data, charts, xaxisDataKey, styles }) => {
   return (
-    <ResponsiveContainer width='100%' height='100%'>
+    <ResponsiveContainer width="100%" height="100%">
       <ComposedChart width={800} height={400} data={data}>
-        <XAxis dataKey={xaxisDataKey}/>
+        <XAxis dataKey={xaxisDataKey} />
         <YAxis tickFormatter={(tick) => formatter.format(tick)} width={30} />
-        <CartesianGrid strokeDasharray='3 3' />
+        <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
         <Legend />
         {charts?.map((chart, index) => {
           if (chart.type === "bar") {
-            return <Bar key={index} dataKey={chart.dataKey} fill={(styles && styles['bar']?.fill) ?? '#8884d8'} />;
+            return (
+              <Bar
+                key={index}
+                dataKey={chart.dataKey}
+                fill={(styles && styles["bar"]?.fill) ?? "#8884d8"}
+              />
+            );
           } else if (chart.type === "line") {
             return (
               <Line
-                type='monotone'
+                type="monotone"
                 key={index}
                 dataKey={chart.dataKey}
-                stroke={(styles && styles['line']?.stroke) ?? '#82ca9d'}
+                stroke={(styles && styles["line"]?.stroke) ?? "#82ca9d"}
+                name={chart.name}
               />
             );
           } else if (chart.type === "area") {
             return (
               <Area
                 key={index}
-                type='monotone'
+                type="monotone"
                 dataKey={chart.dataKey}
-                stroke={(styles && styles['area']?.stroke) ?? '#ffc658'}
-                fill={(styles && styles['area']?.fill) ?? '#ffc658'}
+                stroke={(styles && styles["area"]?.stroke) ?? "#ffc658"}
+                fill={(styles && styles["area"]?.fill) ?? "#ffc658"}
+                name={chart.name}
               />
             );
           }
